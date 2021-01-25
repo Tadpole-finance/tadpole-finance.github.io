29e949e72754bb0ea451ebd711a01794f7ff2de9
yulfan_s <yulfan@mail.com>
2021-01-25 13:17:50 +0700

    add automatically updating footer copyright year

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -11,12 +11,43 @@ fetch("./header.html")
     if(hideMetamask) $('.metamask-container').remove();
   });
 
+  const loadHtml = ((text, dest, replace = false) => {
+		if (typeof dest == 'string') {
+			dest = document.querySelector(dest);
+		}
+		const p = new DOMParser();
+		const doc = p.parseFromString(text, 'text/html');
+		const frag = document.createDocumentFragment();
+		while (doc.body.firstChild) {
+			frag.appendChild(doc.body.firstChild);
+		}
+		// handle script tags
+		const ret = Promise.all(Array.from(frag.querySelectorAll('script'), script => new Promise(resolve => {
+			const scriptParent = script.parentNode || frag;
+			const newScript = document.createElement('script');
+			if (script.src) {
+				newScript.addEventListener('load', e => resolve({ src: script.src, loaded: true }));
+				newScript.addEventListener('error', e => resolve({ src: script.src, loaded: false}));
+				newScript.src = script.src;
+			} else {
+				newScript.textContent = script.textContent;
+				resolve({ src: false, loaded: true });
+			}
+			scriptParent.replaceChild(newScript, script);
+		})));
+		if (replace) {
+			dest.innerHTML = '';
+		}
+		dest.appendChild(frag);
+		return ret;
+	});
+  
 fetch("./footer.html")
   .then(response => {
     return response.text()
   })
   .then(data => {
-    document.querySelector("footer").innerHTML = data;
+    loadHtml(data, "footer")
   });
   
 
