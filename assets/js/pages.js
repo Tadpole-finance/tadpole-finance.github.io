662632fe03b7e500ad5a68c04da95333e27b6c95
tadpole-dev <ngkongs+tadpole@gmail.com>
2021-04-16 19:53:12 +0700

    put load header after document load

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -2,22 +2,7 @@ if (location.protocol !== 'https:' && location.hostname !== "localhost") { //for
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
 }
 
-fetch("./header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-    if(hideMetamask) $('.metamask-container').remove();
-  });
 
-fetch("./footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
-  });
   
 
 var development_alert = function(){
@@ -34,4 +19,21 @@ $(function(){
 	canvs.width = window.innerWidth;
 	canvs.height = window.innerHeight;
 	$('#fishHolder').fishAnimation();
+	
+	fetch("./header.html")
+	  .then(response => {
+		return response.text()
+	  })
+	  .then(data => {
+		document.querySelector("header").innerHTML = data;
+		if(hideMetamask) $('.metamask-container').remove();
+	  });
+
+	fetch("./footer.html")
+	  .then(response => {
+		return response.text()
+	  })
+	  .then(data => {
+		document.querySelector("footer").innerHTML = data;
+	  });
 });
