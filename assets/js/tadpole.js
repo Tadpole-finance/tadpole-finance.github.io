149187b3cde2d233f84c8b0adf4b659c4cef2370
Ax7-cmd <faishal.amrullah@bitcoin.co.id>
2021-07-01 01:12:03 +0700

    change tadpole.finance to app.tadpole.finance

--- assets/js/tadpole.js
+++ assets/js/tadpole.js
@@ -1308,7 +1308,7 @@ var addTadToMetamask = async function(){
 		address: ENV.tadAddress, // The address that the token is at.
 		symbol: 'TAD', // A ticker symbol or shorthand, up to 5 chars.
 		decimals: 18, // The number of decimals in the token
-		image: 'http://tadpole.finance/assets/images/new-logo/Logo-Tadpole-128x128px.png', // A string url of the token logo
+		image: 'http://app.tadpole.finance/assets/images/new-logo/Logo-Tadpole-128x128px.png', // A string url of the token logo
 	  },
 	},
 	});
@@ -1334,7 +1334,7 @@ var addTenToMetamask = async function(){
 		address: ENV.cTokens.ten.underlyingAddress, // The address that the token is at.
 		symbol: 'TEN', // A ticker symbol or shorthand, up to 5 chars.
 		decimals: 18, // The number of decimals in the token
-		image: 'http://tadpole.finance/assets/images/tokens/ten_32.png', // A string url of the token logo
+		image: 'http://app.tadpole.finance/assets/images/tokens/ten_32.png', // A string url of the token logo
 	  },
 	},
 	});
